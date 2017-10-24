bf576f811abfec2fdba46ee6d978e2504c58179a
Oleg Kim <oleg.kim@sclgroup.cc>
2017-10-24 17:16:40 +0100

    tweaks

--- PhonePi.py
+++ PhonePi.py
@@ -1,19 +1,46 @@
-from flask import Flask 
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib
+matplotlib.use('macosx')
+import matplotlib.pyplot as plt
+
+from flask import Flask
 from flask_sockets import Sockets
 
 
-app = Flask(__name__) 
+app = Flask(__name__)
 sockets = Sockets(app)
 
-@sockets.route('/accelerometer') 
+@sockets.route('/accelerometer')
 def echo_socket(ws):
-	 f=open("accelerometer.txt","a")
-	 while True:
-		message = ws.receive()
-		print(message) 
-        	ws.send(message)
-		print>>f,message
-	 f.close()
+     f=open("accelerometer.txt","a")
+     plt.figure()
+     data = [[0, 0, 0]]
+     line1, line2, line3 = plt.plot(range(len(data)), data)
+     plt.ylim(-12, 12)
+     plt.ion()
+     plt.show()
+     while True:
+        message = ws.receive()
+        sys.stdout.flush()
+        print(message)
+        plt.pause(0.0001)
+        data.append([float(i) for i in message.split(',')])
+        d = np.array(data)
+        d = pd.rolling_mean(d, 10)
+        x = range(len(d))
+        line1.set_xdata(x)
+        line1.set_ydata(d[:, 0])
+        line2.set_xdata(x)
+        line2.set_ydata(d[:, 1])
+        line3.set_xdata(x)
+        line3.set_ydata(d[:, 2])
+        plt.xlim(0, max(x))
+        plt.draw()
+        ws.send(message)
+        print>>f,message
+     f.close()
 
 
 @sockets.route('/gyroscope')
@@ -25,7 +52,7 @@ def echo_socket(ws):
                 ws.send(message)
                 print>>f,message
          f.close()
-	
+
 @sockets.route('/magnetometer')
 def echo_socket(ws):
          f=open("magnetometer.txt","a")
@@ -96,10 +123,10 @@ def echo_socket(ws):
                 print>>f,message
          f.close()
 
-	
 
-@app.route('/') 
-def hello(): 
+
+@app.route('/')
+def hello():
     return 'Hello World!'
 
 if __name__ == "__main__":
